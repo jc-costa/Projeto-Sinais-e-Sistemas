8199611bdb69eeef33c0500a5610628ab87f3e5a
Augusto Queiroz <asq@cin.ufpe.br>
2021-08-22 10:07:08 -0300

    Visualizações melhores + chess-board
    python main.py -i chess-board.jpg -v

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import argparse
 
 parser = argparse.ArgumentParser()
 parser.add_argument("-i", "--image", type=str, required=True)
+parser.add_argument("-v", "--visualize", action="store_true")
 
 def detectBlur(image, frequencyThreshold, blurrinessThreshold, visualize=False):
     height, width = image.shape
@@ -16,9 +17,10 @@ def detectBlur(image, frequencyThreshold, blurrinessThreshold, visualize=False):
 
     if visualize:
         # compute the magnitude spectrum of the transform
-        magnitude = 20 * np.log(np.abs(transformedAndShiftedImage))
+        magnitude = 20 * np.log(np.abs(transformedImage))
+        shiftedMagnitude = 20 * np.log(np.abs(transformedAndShiftedImage))
         # display the original input image
-        (fig, ax) = plt.subplots(1, 2, )
+        (fig, ax) = plt.subplots(1, 3, )
         ax[0].imshow(image, cmap="gray")
         ax[0].set_title("Input")
         ax[0].set_xticks([])
@@ -28,12 +30,41 @@ def detectBlur(image, frequencyThreshold, blurrinessThreshold, visualize=False):
         ax[1].set_title("Magnitude Spectrum")
         ax[1].set_xticks([])
         ax[1].set_yticks([])
+        # display the magnitude image
+        ax[2].imshow(shiftedMagnitude, cmap="gray")
+        ax[2].set_title("Shifted Magnitude Spectrum")
+        ax[2].set_xticks([])
+        ax[2].set_yticks([])
         # show our plots
         plt.show()
 
     transformedAndShiftedImage[cY - frequencyThreshold:cY + frequencyThreshold, cX - frequencyThreshold:cX + frequencyThreshold] = 0
     transformedImage = np.fft.ifftshift(transformedAndShiftedImage)
     reconstructedImage = np.fft.ifft2(transformedImage)
+
+    if visualize:
+        # compute the magnitude spectrum of the transform
+        magnitude = 20 * np.log(np.abs(transformedImage))
+        shiftedMagnitude = 20 * np.log(np.abs(transformedAndShiftedImage))
+        # display the original input image
+        (fig, ax) = plt.subplots(1, 3, )
+        ax[0].imshow(shiftedMagnitude, cmap="gray")
+        ax[0].set_title("Shifted Magnitude Spectrum")
+        ax[0].set_xticks([])
+        ax[0].set_yticks([])
+        # display the magnitude image
+        ax[1].imshow(magnitude, cmap="gray")
+        ax[1].set_title("Magnitude Spectrum")
+        ax[1].set_xticks([])
+        ax[1].set_yticks([])
+        # display the magnitude image
+        ax[2].imshow(np.abs(reconstructedImage), cmap="gray")
+        ax[2].set_title("Reconstructed Input")
+        ax[2].set_xticks([])
+        ax[2].set_yticks([])
+        # show our plots
+        plt.show()
+
 
     magnitude = 20 * np.log(np.abs(reconstructedImage))
     mean = np.mean(magnitude)
@@ -46,4 +77,4 @@ originalImage = cv2.imread(args.image)
 originalImage = imutils.resize(originalImage, width=500)
 grayScaleImage = cv2.cvtColor(originalImage, cv2.COLOR_BGR2GRAY)
 
-print(detectBlur(grayScaleImage, frequencyThreshold=60, blurrinessThreshold=10))
+print(detectBlur(grayScaleImage, frequencyThreshold=60, blurrinessThreshold=10, visualize=args.visualize))
